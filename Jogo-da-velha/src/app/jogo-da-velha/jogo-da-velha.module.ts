39cdeec8623c27aae9a8db8deaed2ab88380833a
Moraes <wmarcono@globo.com>
2020-04-06 15:57:10 -0300

    criado componente

--- Jogo-da-velha/src/app/jogo-da-velha/jogo-da-velha.module.ts
+++ Jogo-da-velha/src/app/jogo-da-velha/jogo-da-velha.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { JogoDaVelhaComponent } from './jogo-da-velha.component';
-
+import { JogoDaVelhaService } from './shared'
 
 
 @NgModule({
@@ -16,6 +16,10 @@ import { JogoDaVelhaComponent } from './jogo-da-velha.component';
 
   exports: [
     JogoDaVelhaComponent
+  ],
+
+  providers: [
+    JogoDaVelhaService
   ]
 })
 
